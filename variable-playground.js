a91b2783fddf6476137784c9c9822bf80711dcc8
Gene Fernandez <gdf@comcast.net>
2017-07-24 20:18:32 -0400

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+
+
+// var person = {
+//
+//   name: "Gene",
+//   age: 21
+// };
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name: "Gene",
+//   //   age: 50
+//   // };
+//   obj.age = 50;
+//
+// }
+// updatePerson (person);
+// console.log (person);
+
+
+var grades = [70,88,90];
+
+function addGrades (gradesArr) {
+  gradesArr.push (55);
+  debugger;
+}
+
+addGrades (grades);
+console.log (grades);
