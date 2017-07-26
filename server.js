a7e73c850b4fa1ea55cd6bd76bfdb03f7074dee8
Gene Fernandez <gdf@comcast.net>
2017-07-25 21:38:17 -0400

    Final static todos

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 });
 
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -24,6 +24,10 @@ app.get('/todos', function (req, res) {
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length >0 ) {
+	filteredTodos = _.filter(filteredTodos, function(todo){return todo.description.indexOf(queryParams.q) > -1; });
+}
+
 	res.json(filteredTodos);
 
 });
